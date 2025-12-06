aaec159ab98ab4574853a2757c307785d09287d5
Felix-Hz <feluhv@outlook.es>
2025-12-07 12:50:19 +1300

    fiat lux

---
+++ README.md
@@ -0,0 +1,22 @@
+```
+_
+_ __ ___   ___  _ __   ___ | | ___   __ _
+| '_ ` _ \ / _ \| '_ \ / _ \| |/ _ \ / _` |
+| | | | | | (_) | | | | (_) | | (_) | (_| |
+|_| |_| |_|\___/|_| |_|\___/|_|\___/ \__, |
+                                      |___/
+```
+
+# How to
+
+```
+# UNIX
+cd /path/to/monolog
+chmod +x ./run.sh
+./run.sh
+
+# Windows
+# Git gives you unix commands, and bash support. 
+# If it's already on your PATH, you should theoretically be able to run the script directly.
+bash ./run.sh
+```
---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "⚙️ Installing uv..."
+install_uv
+divider
+
+echo "⚙️ Activating virtual environment..."
+source .venv/bin/activate
+divider
+
+echo "⚙️ Installing dependencies..."
+uv sync
+divider
+
+echo "⚙️ Checking for outstanding migrations..."
+python manage.py makemigrations
+python manage.py migrate
+divider
+
+echo "⚙️ Running server..."
+python manage.py runserver
+
+# Utils
+function divider() { printf '_%.0s' {1..16};  printf '\n\n'; }
+function install_uv(){
+    if ! which uv > /dev/null; then
+        if [ "$(uname)" == "Darwin" ] || [ "$(uname)" == "Linux" ]; then
+            curl -LsSf https://astral.sh/uv/install.sh | sh
+        elif [ "$(uname)" == "Windows" ]; then
+            powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+        else
+            echo "⚠️ Unsupported operating system"
+            exit 1
+        fi
+    else
+        echo "✅ uv is already installed"
+    fi
+}
