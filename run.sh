177a296d3557bda9f57ac637d8cedd5a25dc87aa
Felix-Hz <feluhv@outlook.es>
2025-12-07 13:17:11 +1300

    chore: check py version

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
- 
+
 # Utils
-function divider() { printf '_%.0s' {1..16};  printf '\n\n'; }
-function install_uv(){
+function divider  { printf '_%.0s' {1..16};  printf '\n\n'; }
+function install_uv {
     if ! which uv > /dev/null; then
         if [ "$(uname)" == "Darwin" ] || [ "$(uname)" == "Linux" ]; then
             curl -LsSf https://astral.sh/uv/install.sh | sh
@@ -16,6 +16,16 @@ function install_uv(){
         echo "✅ uv is already installed"
     fi
 }
+function validate_py_version {
+    local VERSION="$(python --version | awk '{print $2}' | cut -d'.' -f1-2)"
+    if (( $(echo "$VERSION >= 3.12" | bc -l) )); then
+        echo "✅ Supported"
+    else
+        echo "⚠️ Unsupported"
+    fi
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 # Execution
 echo "⚙️ Installing uv..."
@@ -24,6 +34,10 @@ divider
 
 echo "⚙️ Activating virtual environment..."
 source .venv/bin/activate
+divider
+
+echo "⚙️ Validating Python version..."
+validate_py_version
 divider
 
 echo "⚙️ Installing dependencies..."
@@ -37,4 +51,6 @@ divider
 
 echo "⚙️ Running server..."
 python manage.py runserver
+divider
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
